cc8d147ee7c4548000faf17060fee47e2d41c135
cedric e goddard <8ceg6@comcast.net>
2019-12-14 05:18:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,3 +8,14 @@
 #     end
     
 # end 
+
+def my_collect(empty_array)
+  i=0
+  new_empty_array=[]
+  while i < empty_array.length 
+  new_empty_array<< yield(empty_array[i])
+  i+=1 
+end 
+new_empty_array
+
+end
