d8561222a9bbc8db662ce3c5280a7cb37e953f1b
cedric e goddard <8ceg6@comcast.net>
2019-12-14 05:19:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,11 @@
-# def my_collect(array)
-#     new_array =[]
-#     i = 0 
-#     while i < array.length
-#       yield(array[i])
-#       i += 1
-#     new_array << array
-#     end
+def my_collect(array)
+    new_array =[]
+    i = 0 
+    while i < array.length
+      yield(array[i])
+      i += 1
+    new_array << array
+    end
     
-# end 
-
-def my_collect(empty_array)
-  i=0
-  new_empty_array=[]
-  while i < empty_array.length 
-  new_empty_array<< yield(empty_array[i])
-  i+=1 
 end 
-new_empty_array
 
-end
